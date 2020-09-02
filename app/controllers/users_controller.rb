ef511f296aee2ed5af9ec73e25862033ca2ae5e1
TaitaT <taita.omori@gmail.com>
2020-09-02 12:32:13 +0900

    一覧ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def questions_list
+  end
+
+  def answer_requests
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "questions#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      get 'questions_list','answer_requests'
+    end
+  end
+
+  
 end
