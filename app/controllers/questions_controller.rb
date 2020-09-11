e74a31782b2a762dd7778c7fbe471493b3ca47e7
TaitaT <taita.omori@gmail.com>
2020-09-11 19:33:17 +0900

    質問削除機能の実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,7 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @answers = Answer.where(question_id: params[:id])
+    @posted = Post.where(question_id: @question.id)
   end
 
 
