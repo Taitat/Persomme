4cef91ed0a5e19d07276898a77622b869f0682f7
TaitaT <taita.omori@gmail.com>
2020-09-18 12:43:41 +0900

    プロフィール画像の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
+  has_one_attached :image
   has_many :questions
   has_many :requests
   has_many :answers
