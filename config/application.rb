33c304bbe43d1d7525967ae6f1d5e5729eab5432
TaitaT <taita.omori@gmail.com>
2020-09-21 21:28:44 +0900

    フォントの設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Persommelier
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
     config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
       html_tag
     end
